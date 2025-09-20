0ecfda79ecc474a42810c1993219a50c486e1c14
3Bmodular <3bmod@bglab.local>
2025-09-19 21:27:32 -0300

    Aplicar .gitignore e limpar arquivos indesejados

--- bdBiblioteca/Models/Genero.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace bdBiblioteca.Models
-{
-    public class Genero
-    { 
-        public int id { get; set; }
-        public string nome { get; set; }
-        public string criado_em { get; set;}
-    }
-}
